8a8d90a54b8b5945a0e967a2a4d9136a870fbd4a
Lukáš Vasek <bilaak@gmail.com>
2019-11-29 13:32:31 +0100

    added configuration for authorization service hidden behind gateway

--- src/main/java/com/github/bilakpoc/springgatewayoauth2permittedenpointissue/SecurityConfiguration.java
+++ src/main/java/com/github/bilakpoc/springgatewayoauth2permittedenpointissue/SecurityConfiguration.java
@@ -2,18 +2,37 @@ package com.github.bilakpoc.springgatewayoauth2permittedenpointissue;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
 
+
+
     @Bean
+    @Order(1)
+    public SecurityWebFilterChain authSpringSecurityFilterChain(final ServerHttpSecurity http) {
+        return http
+                .anonymous(withDefaults())
+                .cors(ServerHttpSecurity.CorsSpec::disable)
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .headers(headers -> headers.frameOptions(opt -> opt.mode(XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN)))
+                .securityMatcher(new PathPatternParserServerWebExchangeMatcher("/auth/**"))
+                //.securityMatcher(new PathPatternParserServerWebExchangeMatcher("/permitted.html"))
+                .build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityWebFilterChain springSecurityFilterChain(final ServerHttpSecurity http) {
         return http
-                .authorizeExchange(e ->
-                        e.pathMatchers("/auth/**", "/permitted.html").permitAll()
-                                .anyExchange().authenticated())
+                .authorizeExchange(e -> e.anyExchange().authenticated())
                 .cors(ServerHttpSecurity.CorsSpec::disable)
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
